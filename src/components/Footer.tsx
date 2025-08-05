dbd84a0567e7eff61802073a7d1f7dc22285a6da
Shamsul <48136141+shkms11@users.noreply.github.com>
2025-08-06 04:31:18 +0600

    update contents of footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
+import "./Footer.css";
 function Footer() {
     return (
-        <div>
-            <h1>footer</h1>
+        <div className="bg-blue-500 flex-column">
+            <h1>connect with us</h1>
+            <h1>trending now</h1>
+            <h1>about us</h1>
+            <h1>help</h1>
         </div>
     );
 }
