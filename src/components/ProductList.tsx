abafc669be8a5357d0d7bcf04751390636f1ca51
Shamsul <48136141+shkms11@users.noreply.github.com>
2025-09-08 09:53:17 +0600

    update margin and flex

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -3,10 +3,14 @@ import Product from "./Product";
 function ProductList() {
     const items = Array.from({ length: 10 });
     return (
-        <div className="p-4">
+        <div className="p-4 mr-20 ml-20">
             <div className="flex flex-wrap">
                 {items.map((_, index) => (
-                    <div key={index} className="w-1/4 ">
+                    <div
+                        key={index}
+                        className="lg:w-1/4
+                    md:w-1/3 "
+                    >
                         <Product />
                     </div>
                 ))}
