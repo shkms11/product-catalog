4fe63dfa0178e7849e9089d1282a17ca09e44536
Shamsul <48136141+shkms11@users.noreply.github.com>
2025-09-08 09:45:19 +0600

    update product list for showcasing a catagory of products

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,11 +1,17 @@
 import Product from "./Product";
 
 function ProductList() {
+    const items = Array.from({ length: 10 });
     return (
-        <>
-            <h1>list of the products for a catagory</h1>
-            <Product />
-        </>
+        <div className="p-4">
+            <div className="flex flex-wrap">
+                {items.map((_, index) => (
+                    <div key={index} className="w-1/4 ">
+                        <Product />
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 }
 
